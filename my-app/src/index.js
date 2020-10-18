486c946363f227154f25a5bb2ff719acc4fb4205
Ian McGregor <ianmcgregor@Ians-MacBook-Pro.local>
2020-10-17 20:18:30 -0400

    Introducing JSX

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,4 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello, World</h1>, document.getElementById("root"));
+function formatName(user) {
+  return user.firstName + " " + user.lastName;
+}
+
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hello, {formatName(user)}</h1>;
+  }
+
+  return <h1>Hello, Stranger</h1>;
+}
+
+const user = {
+  firstName: "Ian",
+  lastName: "McGregor",
+};
+
+const element = (
+  <div className="element">
+    {getGreeting(user)}
+    <h2>Good to see you here!</h2>
+  </div>
+);
+
+const sameElement = React.createElement(
+  "div",
+  { className: "element" },
+  getGreeting(user),
+  <h2>Good to see you here!</h2>
+);
+
+ReactDOM.render(sameElement, document.getElementById("root"));
